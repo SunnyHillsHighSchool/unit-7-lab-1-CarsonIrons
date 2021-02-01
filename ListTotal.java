73aa49741e552a9f98bca501034904dc257f6b77
Carson Irons <600020180@fjuhsd.org>
2021-02-01 02:56:50 +0000

    added method to return sum of Integers in the arrayList

--- ListTotal.java
+++ ListTotal.java
@@ -8,8 +8,21 @@ import java.util.ArrayList;
 
 public class ListTotal
 {
- public static int total( List<Integer> ray )
+ //Method for adding the elements of the arraylist
+ //By Carson Irons
+ //P2
+//Method header with integer arrayList parameter and int return type
+public static int total( List<Integer> ray )
  {
-  return 0;
+//Declare and initialize int sum
+int sum =0;
+//loop through the elements of the code with a for each (or for) loop
+for(int i=0; i<ray.size();i++){
+//add current element to sum
+sum+=ray.get(i);
+//end loop
+}
+//return sum
+return sum;  
  }
 }
